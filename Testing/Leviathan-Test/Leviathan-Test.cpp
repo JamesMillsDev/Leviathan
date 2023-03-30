36b4d06b96670f12d9e6d59c248e5052573d971a
James Mills <jmills@tunacornstudios.com>
2023-03-30 21:27:15 +1100

    Initial project setup

---
+++ Testing/Leviathan-Test/Leviathan-Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LeviathanTest
+{
+	TEST_CLASS(LeviathanTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
